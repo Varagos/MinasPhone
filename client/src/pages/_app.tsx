bdbae151165f12b3c09f34cdb998a094b3b9aa47
Markos Girgis <markos@bitloops.com>
2023-11-10 16:30:06 +0200

    remove supertokens

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -7,28 +7,18 @@ import Footer from '@/components/Footer/Footer';
 import { CartProvider } from '@/context/CartProvider';
 import '../styles/globals.css';
 import React from 'react';
-import SuperTokensReact, { SuperTokensWrapper } from 'supertokens-auth-react';
-import { frontendConfig } from '@/config/supertokens/frontendConfig';
-import SuperTokensWebJs from 'supertokens-web-js';
-
-if (typeof window !== 'undefined') {
-  // we only want to call this init function on the frontend, so we check typeof window !== 'undefined'
-  // SuperTokensReact.init(frontendConfig());
-}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <CartProvider>
-      <SuperTokensWrapper>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Navbar />
-          <main style={{ minHeight: '80vh' }}>
-            <Component {...pageProps} />
-          </main>
-          <Footer />
-        </ThemeProvider>
-      </SuperTokensWrapper>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <main style={{ minHeight: '80vh' }}>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </ThemeProvider>
     </CartProvider>
   );
 }

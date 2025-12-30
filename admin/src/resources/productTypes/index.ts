39addc20b4fa11b0687901069c0e88361027bee2
Markos Girgis <markos@bitloops.com>
2025-12-30 10:27:18 +0200

    feat: update icons for categories, product types, and products

--- admin/src/resources/productTypes/index.ts
+++ admin/src/resources/productTypes/index.ts
@@ -1,11 +1,11 @@
 import { ProductTypeList } from './ProductTypeList';
 import { ProductTypeCreate } from './ProductTypeCreate';
 import { ProductTypeEdit } from './ProductTypeEdit';
-import CategoryIcon from '@mui/icons-material/Category';
+import TuneIcon from '@mui/icons-material/Tune';
 
 export default {
-    list: ProductTypeList,
-    create: ProductTypeCreate,
-    edit: ProductTypeEdit,
-    icon: CategoryIcon,
+  list: ProductTypeList,
+  create: ProductTypeCreate,
+  edit: ProductTypeEdit,
+  icon: TuneIcon,
 };
--- admin/src/resources/products/index.tsx
+++ admin/src/resources/products/index.tsx
@@ -3,11 +3,12 @@ import { ProductCreate } from './ProductCreate';
 import InventoryIcon from '@mui/icons-material/Inventory';
 import { ProductEdit } from './ProductEdit';
 import ProductList from './ProductList';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 
 const resource = {
   list: ProductList,
   create: ProductCreate,
   edit: ProductEdit,
-  icon: InventoryIcon, // ProductIcon,
+  icon: PhoneIphoneIcon,
 };
 export default resource;
